9c2e3a540aa31a453ccb5c3bc32bb35aca23fe58
htquyen53 <quinnquinnisme@gmail.com>
2023-05-24 00:52:55 +0700

    nop bai tap ss11

--- module2/src/ss11/count_word_by_map/MainMap.java
+++ module2/src/ss11/count_word_by_map/MainMap.java
@@ -1,7 +1,27 @@
 package ss11.count_word_by_map;
 
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeMap;
+
 public class MainMap {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập chuỗi cần kiểm tra: ");
+        String string = scanner.nextLine().toLowerCase();
+        Map<String, Integer> mapWord = new TreeMap<>();
+        String[] arrString = string.split(" ");
+        for (String s : arrString) {
+            if (!mapWord.containsKey(s)) {
+                mapWord.put((s), 1);
+            } else {
+                mapWord.put((s), mapWord.get(s) + 1);
+            }
+        }
+        Set<String> keySet = mapWord.keySet();
+        for (String key : keySet) {
+            System.out.println(key + " - " + mapWord.get(key));
+        }
     }
 }
