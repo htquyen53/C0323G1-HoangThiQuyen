a8cf04a98d7c316d8b53bead260dfcdf61ecb32f
htquyen53 <quinnquinnisme@gmail.com>
2023-05-31 21:32:14 +0700

    nop bai tap casestudy

--- module2/src/case_study/furama_resort/model/services/Booking.java
+++ module2/src/case_study/furama_resort/model/services/Booking.java
@@ -1,4 +1,5 @@
 package case_study.furama_resort.model.services;
 
 public class Booking {
+
 }
---
+++ module2/src/case_study/furama_resort/view/View.java
@@ -0,0 +1,9 @@
+package case_study.furama_resort.view;
+
+import case_study.furama_resort.controller.FuramaController;
+
+public class View {
+    public static void main(String[] args) {
+        FuramaController.displayMainMenu();
+    }
+}
