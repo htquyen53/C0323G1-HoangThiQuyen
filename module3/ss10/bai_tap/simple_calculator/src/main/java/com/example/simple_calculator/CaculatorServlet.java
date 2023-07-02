2840ee1633675a56e16940e14fc1e33c2232821c
htquyen53 <quinnquinnisme@gmail.com>
2023-07-02 13:13:57 +0700

    nop bai tap ss10

--- module3/ss10/bai_tap/simple_calculator/src/main/java/com/example/simple_calculator/CaculatorServlet.java
+++ module3/ss10/bai_tap/simple_calculator/src/main/java/com/example/simple_calculator/CaculatorServlet.java
@@ -4,16 +4,49 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.concurrent.ExecutionException;
 
 @WebServlet(name = "CaculatorServlet", value = "/simple-calculator")
 public class CaculatorServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        double firstOperand = Double.parseDouble(request.getParameter("first operand"));
+        double secondOperand = Double.parseDouble(request.getParameter("second operand"));
+        char operator = request.getParameter("operator").charAt(0);
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<h1> Result: </h1>");
+        double result = 0;
+        switch (operator) {
+            case ('+'):
+                result = firstOperand + secondOperand;
+                writer.println(firstOperand + " " + operator + " " + secondOperand + " =" + result);
+                break;
+            case ('-'):
+                result = firstOperand - secondOperand;
+                writer.println(firstOperand + " " + operator + " " + secondOperand + " =" + result);
+                break;
+            case ('*'):
+                result = firstOperand * secondOperand;
+                writer.println(firstOperand + " " + operator + " " + secondOperand + " =" + result);
+                break;
+            case ('/'):
+                try {
+                    result = firstOperand / secondOperand;
+                } catch (Exception e) {
+                    writer.println("Error: " + e.getMessage());
+                }
+                writer.println(firstOperand + " " + operator + " " + secondOperand + " =" + result);
+                break;
+            default:
+                writer.println("You enter wrong operator format!");
+        }
+        writer.println("</html>");
     }
 }
--- module3/ss10/bai_tap/simple_calculator/src/main/webapp/index.jsp
+++ module3/ss10/bai_tap/simple_calculator/src/main/webapp/index.jsp
@@ -1,4 +1,5 @@
 <%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,11 +7,36 @@
 </head>
 <body>
 <h1><%= "Simple Calculator" %>
-    <form action="/simple-calculator" method="post">
-        <lable ></lable>
-    </form>
 </h1>
-<br/>
-<a href="simple-calculator">Simple Calculator</a>
+<form action="/simple-calculator" method="post">
+    <fieldset>
+        <legend>Calculator</legend>
+        <table>
+            <tr>
+                <td>First oparand:</td>
+                <td><input type="text" name="first operand" placeholder="enter here..."/></td>
+            </tr>
+            <tr>
+                <td>Operator:</td>
+                <td><select disabled="disabled" name="operator">
+                    <option name="addition" value="+">Addition</option>
+                    <option name="subtraction" value="-">Subtraction</option>
+                    <option name="multiplication" value="*">Multiplication</option>
+                    <option name="division" value="/">Division</option>
+                </select>
+                </td>
+            </tr>
+            <tr>
+                <td>Second operand</td>
+                <td><input type="text" name="second operand" placeholder="enter here..."/></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="submit" id="submit" name="Calculate"/>
+                </td>
+            </tr>
+        </table>
+    </fieldset>
+</form>
 </body>
 </html>
