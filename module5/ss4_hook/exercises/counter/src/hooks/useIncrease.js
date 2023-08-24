27b35e168c89b0f33edf324561c63f3085395fbb
htquyen53 <quinnquinnisme@gmail.com>
2023-08-24 11:37:11 +0700

    nop bt fix

--- module5/ss4_hook/exercises/counter/src/hooks/useIncrease.js
+++ module5/ss4_hook/exercises/counter/src/hooks/useIncrease.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 export default function useIncrease() {
     const [count, setCount] = useState(0);
     const increase = (addAmount) => {
-        setCount(count + addAmount);
+        setCount((pre) => (pre + addAmount));
     }
     return [count, increase];
 }
