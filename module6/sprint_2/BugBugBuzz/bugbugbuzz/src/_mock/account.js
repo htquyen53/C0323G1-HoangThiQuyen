3c92a1bf8eac9ab0a0f2f48416370a05bdf394d6
htquyen53 <quinnquinnisme@gmail.com>
2023-10-05 05:20:17 +0700

    nop bai tap sprint 2

---
+++ module6/sprint_2/BugBugBuzz/bugbugbuzz/src/_mock/account.js
@@ -0,0 +1,9 @@
+// ----------------------------------------------------------------------
+
+const account = {
+  displayName: 'Quinn Hoang',
+  email: 'quinnquinnisme@gmail.com',
+  photoURL: '/assets/images/avatars/avatar_default.jpg',
+};
+
+export default account;
